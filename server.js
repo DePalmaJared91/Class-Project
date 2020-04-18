9f04c5c43f7078a5b05c9927505f1db7c8e6cb39
Jared DePalma <depalmajared91@gmail.com>
2020-04-18 11:57:40 -0600

    3rd

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  app.get("/reserve", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  var reserve = [];
+  var table = [];
+  
