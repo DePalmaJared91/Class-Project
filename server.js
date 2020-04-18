b35a8d9ce380e2e6148d8a687556b2c73ca73af2
Jared DePalma <depalmajared91@gmail.com>
2020-04-18 12:36:28 -0600

    5th

--- server.js
+++ server.js
@@ -19,7 +19,15 @@ app.get("/", function(req, res) {
     // res.send("Welcome to the Star Wars Page!")
     res.sendFile(path.join(__dirname, "reserve.html"));
   });
+  app.post("/api/reserve", function(req, res){
+      var newReservation = req.body;
+      newReservation.routeName = req.body;
+      newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  })
 
   var reserve = [];
   var table = [];
-  
+
+  app.listen(PORT, function()
+  {console.log("Listening")});
